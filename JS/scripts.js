fc9a8deba91fc5500f9800a9d7198288a5a3880a
Jamie Aquino <jamie.aquino33@gmail.com>
2019-10-16 14:41:46 -0500

    Add weather card and started styling in CSS

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,16 +1,29 @@
 (function () {
-    //getWeatherInfo();
-    geocode();
+    //Submit button event handler
+    $("#submit").click(function() {
+        //Get the value the user has entered un the search bar and store it
+        const searchLocation = $("#searchBar").val();
+        //Call the geocode function and pass in the value
+        geocode(searchLocation);
+        //Clear out the search bar
+        $("#searchBar").val("");
+    });
 })();
 
 //Function to connect to the Dark Aky API and get weather data
-function getWeatherInfo() {
+function getWeatherInfo(latitude, longitude, city, state) {
 
     //Base-URL/APIKey/Latitude,Longitude
 
-    $.ajax("https://api.darksky.net/forecast/" + darkSkyKey + "/37.8267,-122.4233", { dataType: "jsonp"})
+    $.ajax("https://api.darksky.net/forecast/" + darkSkyKey + "/" + latitude + "," + longitude, { dataType: "jsonp"})
     .done(function(data) {
         console.log(data);
+
+        //Cuurent temperature 
+
+        //Probability of percipitation
+
+        //High and low temps for the current day
     })
     .fail(function(error) {
         console.log(error);
@@ -22,13 +35,24 @@ function getWeatherInfo() {
 
 
 //Function to connect to the MapQuest Geocoding API and get geocoding data
-function geocode() {
+function geocode(location) {
 
     //Base-URL + APIKey + &Location= + Address
-    
-    $.ajax("http://www.mapquestapi.com/geocoding/v1/address?key=" + mapQuestKey + "&location=Washington,DC")
+
+    $.ajax("http://www.mapquestapi.com/geocoding/v1/address?key=" + mapQuestKey + "&location=" + location)
     .done(function(data) {
-        console.log(data);
+        //Get the lat and lng from the response
+        let locations = data.results[0].locations[0];
+
+        let lat = locations.latLng.lat;
+        let lng = locations.latLng.lng;
+
+        //Get city and state so we can display it to the user later
+        let city = locations.adminArea5;
+        let state = locations.adminArea3;
+
+        //Pass the lat and lng to our getWeatherInfo function
+        getWeatherInfo(lat, lng, city, state);
     })
     .fail(function(error) {
         console.log(error);
